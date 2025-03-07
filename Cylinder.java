8b4e688e3f37622b7290d151a7ea79664d24176d
vedants27 <vedantss27@gmail.com>
2025-03-07 23:30:36 +0530

    Added constructor to initialize Cylinder with shape name, radius, and height.

--- Cylinder.java
+++ Cylinder.java
@@ -2,4 +2,10 @@
 
 class Cylinder extends Shape implements Volume {
     private double radius, height;
+
+    public Cylinder(double radius, double height) {
+        setShapeName("Cylinder");
+        this.radius = radius;
+        this.height = height;
+    }
 }
