36929016c3cc92c2d0632bd3d8b0b8705bdb3590
vedants27 <vedantss27@gmail.com>
2025-03-07 23:26:26 +0530

    Added constructor to initialize Sphere with shape name and radius.

--- Sphere.java
+++ Sphere.java
@@ -1,5 +1,10 @@
 //Sphere.java 
 class Sphere extends Shape implements Volume {
     private double radius;
+
+    public Sphere(double radius) {
+        setShapeName("Sphere");
+        this.radius = radius;
+    }
 }
 
