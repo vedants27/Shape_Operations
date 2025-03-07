67d2ac419814a58d15c2391ee147971b48c97498
vedants27 <vedantss27@gmail.com>
2025-03-07 23:12:48 +0530

    Added calculatePerimeter method to calculate perimeter.

--- Circle.java
+++ Circle.java
@@ -10,4 +10,9 @@ class Circle extends Shape {
     // Method to calculate and return the area ofcircle
     public double calculateShape() {
         return Math.PI * radius * radius;
+    }
+    //Method to calculate perimeter of circle.
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
 }
