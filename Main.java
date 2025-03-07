a19cb5bf64f0a2965fd4a228166e12332627309f
vedants27 <vedantss27@gmail.com>
2025-03-07 22:50:05 +0530

    Updated Main.java by adding menu for shape selection.

--- Main.java
+++ Main.java
@@ -8,6 +8,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
+        // Menu for user to select a shape.
+        do {
+            System.out.println("\nSelect Shape:");
+            System.out.println("1. Circle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Square");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Equilateral Pyramid");
+            System.out.println("0. Exit");
+            System.out.print("Enter choice: ");
+            choice = scanner.nextInt();
+
     }
 }
 
