b9a7d2902a81edd51e127031c93c7b39ad8db169
vedants27 <vedantss27@gmail.com>
2025-03-07 23:04:32 +0530

    Added setShapeName method to assign shape names.

--- Shape.java
+++ Shape.java
@@ -1,5 +1,10 @@
 //Shape.java
 abstract class Shape {
     protected String shapeName;
+
+    // Setter to assign shape name.
+    public void setShapeName(String shape) {
+        this.shapeName = shape;
+    }
 }
 
