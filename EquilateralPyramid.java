658b0336378f69c5998830f2ed8f1b4833bab287
vedants27 <vedantss27@gmail.com>
2025-03-07 23:35:29 +0530

    Updated EquilateralPyramid.java file.

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -2,4 +2,10 @@
 
 class EquilateralPyramid extends Shape implements Volume {
     private double base, height;
+
+    public EquilateralPyramid(double base, double height) {
+        setShapeName("Equilateral Pyramid");
+        this.base = base;
+        this.height = height;
+    }
 }
